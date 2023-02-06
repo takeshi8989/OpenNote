6b2fce30f88543e0f93d1987f29abd95c442edab
Takeshi Hashimoto <htakeshi0614@gmail.com>
2023-02-06 11:12:19 -0800

    second

---
+++ src/main/java/opennote/OpenNoteApplication.java
@@ -0,0 +1,21 @@
+package opennote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class OpenNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenNoteApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public static String hello(){
+		return "Hello Open Note!";
+	}
+
+}
