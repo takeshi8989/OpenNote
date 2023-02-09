9c77c4426c2dfd8d40b04a44e386e99488f8b685
TAKESHI <109770597+takeshi8989@users.noreply.github.com>
2023-02-09 00:14:11 -0800

    6 feat user create user table and connect to postgres db (#7)
    * make change in gitignore file
    
    * connect to postgres db aws rds
    
    * use environment variables
    
    * edit yml
    
    * edit yml
    
    * edit dockerfile
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * without env
    
    * add json env
    
    * add json env
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * removed env var

---
+++ src/main/java/opennote/User/User.java
@@ -0,0 +1,52 @@
+package opennote.User;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name =  "users")
+public class User {
+    @Id
+    @SequenceGenerator(
+            name = "user_id_sequence",
+            sequenceName = "user_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_id_sequence"
+    )
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+
+    public User(int id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
