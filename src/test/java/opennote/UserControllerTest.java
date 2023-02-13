742bd21a63a75e480d0fda28098fca93374c6b7c
Takeshi Hashimoto <htakeshi0614@gmail.com>
2023-02-13 00:05:36 -0800

    write some tests

---
+++ src/test/java/opennote/UserControllerTest.java
@@ -0,0 +1,113 @@
+package opennote;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import opennote.User.User;
+import opennote.User.UserController;
+import opennote.User.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper mapper;
+
+    @MockBean
+    UserRepository userRepository;
+
+    User user1 = new User(1, "Rayven Yor", "yrayven@gmail.com", "password1");
+    User user2 = new User(2, "David Landup", "ldavid@gmail.com", "password2");
+    User user3 = new User(3, "Jane Doe", "djane@gmail.com", "password3");
+
+    @Test
+    public void getAllUsers_success() throws Exception {
+        List<User> users = new ArrayList<>(Arrays.asList(user1, user2, user3));
+
+        Mockito.when(userRepository.findAll()).thenReturn(users);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/users")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[2].username", is("Jane Doe")));
+    }
+
+    @Test
+    public void getUserById_success() throws Exception {
+        Mockito.when(userRepository.findById(user1.getId())).thenReturn(java.util.Optional.of(user1));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/users/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.username", is("Rayven Yor")));
+    }
+
+    @Test
+    public void createUser_success() throws Exception {
+        record UserRequest(
+                String username,
+                String email,
+                String password
+        ) {}
+        UserRequest user = new UserRequest(
+                "Maricela Sandoval",
+                "smaricela@gmail.com",
+                "password4"
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(user)))
+                .andExpect(status().isOk());
+    }
+
+//    @Test
+//    public void updateUser_success() throws Exception {
+//        Mockito.when(userRepository.findById(user1.getId())).thenReturn(java.util.Optional.of(user1));
+//        record UserRequest(
+//                String username,
+//                String email,
+//                String password
+//        ) {}
+//        UserRequest user = new UserRequest(
+//                "Dixie Montoya",
+//                "mdixie@gmail.com",
+//                "password5"
+//        );
+//
+//        mockMvc.perform(MockMvcRequestBuilders
+//                        .put("/api/v1/users/1")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(toJson(user)))
+//                .andExpect(status().isOk());
+//    }
+
+    public static String toJson(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
