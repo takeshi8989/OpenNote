986e98a5e0cfad5e82db1a9ef3f76ddef5be4ff3
Takeshi Hashimoto <htakeshi0614@gmail.com>
2023-02-16 21:46:08 -0800

    UserService class is responsible to access repository

--- src/main/java/opennote/User/UserRepository.java
+++ src/main/java/opennote/User/UserRepository.java
@@ -1,6 +1,8 @@
 package opennote.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
