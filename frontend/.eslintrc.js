bbe29f2e4728cb72c0d822a66b859982301c3eb3
Takeshi Hashimoto <htakeshi0614@gmail.com>
2023-02-22 12:09:05 -0800

    nextjs directory

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: { browser: true },
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+    "prettier/react",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaFeatures: { jsx: true } },
+  settings: { react: { version: "detect" } },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        arrowParens: "avoid",
+        semi: false,
+        singleQuote: true,
+        trailingComma: "none",
+      },
+    ],
+  },
+};
