309ee8d505a4876d15182667d1b217c98372ce80
Takeshi Hashimoto <htakeshi0614@gmail.com>
2023-02-21 22:10:12 -0800

    added new directory

--- open-note/src/main/java/opennote/Application.java
+++ open-note/src/main/java/opennote/Application.java
@@ -10,4 +10,9 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@GetMapping
+	public String hello(){
+		return "Hello";
+	}
 }
