bbd0a56539d70c9854aa2cef3f35a68fea3569c0
TAKESHI <109770597+takeshi8989@users.noreply.github.com>
2023-03-01 21:59:06 -0800

    feat web add loading animation when file is being loaded (#126)
    * add loading animation to single page pdf
    
    * loading in note page
    
    * display failure messag
    
    * make change in styling

--- frontend/src/components/MultiPagePDF.tsx
+++ frontend/src/components/MultiPagePDF.tsx
@@ -1,16 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
+import { Button, Loading, Text } from "@nextui-org/react";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const MultiPagePDF = ({ url }: { url: string }) => {
   const [numPages, setNumPages] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [loadFailed, setLoadFailed] = useState<boolean>(false);
+
   useEffect(() => {
     pdfjs.getDocument(url).promise.then((doc) => setNumPages(doc.numPages));
   }, [url]);
+
+  const removeLoading = () => {
+    setIsLoading(false);
+  };
+
+  const displayLoadError = () => {
+    setLoadFailed(true);
+  };
+
   return (
     <div className="w-full flex flex-col items-center ">
+      {isLoading && !loadFailed && (
+        <div
+          className="flex justify-center items-center"
+          style={{ width: "300px", height: "300px" }}
+        >
+          <Loading size="lg" />;
+        </div>
+      )}
+      {loadFailed && (
+        <div className="mt-20">
+          <Text className="text-center">Failed to load PDF file.</Text>
+        </div>
+      )}
       {Array.from(Array(numPages), (e, i) => (
-        <Document file={url} key={i} className="my-5">
+        <Document
+          file={url}
+          key={i}
+          className="my-5"
+          onLoadSuccess={removeLoading}
+          loading=""
+          error=""
+          onLoadError={displayLoadError}
+        >
           <Page
             className="w-full"
             pageNumber={i + 1}
