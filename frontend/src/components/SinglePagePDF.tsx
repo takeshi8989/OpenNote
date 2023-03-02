d5cdf5af8a7a4fadfc666e66f9033b14c0b7b55a
Takeshi Hashimoto <htakeshi0614@gmail.com>
2023-03-01 21:38:39 -0800

    add loading animation to single page pdf

--- frontend/src/components/SinglePagePDF.tsx
+++ frontend/src/components/SinglePagePDF.tsx
@@ -1,6 +1,6 @@
 import { Note } from "@/types/note";
-import React from "react";
-import { Link } from "@nextui-org/react";
+import React, { useState } from "react";
+import { Link, Loading } from "@nextui-org/react";
 import { Document, Page, pdfjs } from "react-pdf";
 import { AiOutlineLike } from "react-icons/ai";
 import { BiCommentDetail } from "react-icons/bi";
@@ -10,14 +10,28 @@ import CustomTag from "./tag/CustomTag";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const SinglePagePDF = ({ note }: { note: Note }): JSX.Element => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const removeLoading = () => {
+    setIsLoading(false);
+  };
+
   return (
     <div className="mb-14 mt-14" style={{ width: 300 }}>
       <p className="text-center mb-0 pb-0 text-2xl">{note.title}</p>
       <p className="text-center mt-0">{note.user?.username}</p>
       <Link href={`note/${note.id}`}>
         {/* The first Page of PDF file */}
+
         <div className="relative">
-          <Document file={note.url}>
+          {isLoading && (
+            <div
+              className="flex justify-center items-center"
+              style={{ width: "300px", height: "300px" }}
+            >
+              <Loading size="lg" />;
+            </div>
+          )}
+          <Document file={note.url} onLoadSuccess={removeLoading}>
             <Page
               className="w-full"
               pageNumber={1}
@@ -27,23 +41,27 @@ const SinglePagePDF = ({ note }: { note: Note }): JSX.Element => {
             />
           </Document>
           {/* 2 Tags */}
-          <div className="flex absolute bottom-3 right-1">
-            {note.tags.length > 0 && <CustomTag tag={note.tags[0]} />}
-            {note.tags.length > 1 && <CustomTag tag={note.tags[1]} />}
-          </div>
+          {!isLoading && (
+            <div className="flex absolute bottom-3 right-1">
+              {note.tags.length > 0 && <CustomTag tag={note.tags[0]} />}
+              {note.tags.length > 1 && <CustomTag tag={note.tags[1]} />}
+            </div>
+          )}
         </div>
       </Link>
       {/* View, Like, Download, Commnet */}
-      <div className="flex items-center justify-end mt-1">
-        <TbClick size={20} />
-        <p className="mr-2">123</p>
-        <AiOutlineLike size={24} />
-        <p className="mr-2">{note.likes.length}</p>
-        <GrDownload size={20} />
-        <p className="mr-2">{note.numDownload}</p>
-        <BiCommentDetail size={24} />
-        <p className="mr-2">{note.comments.length}</p>
-      </div>
+      {!isLoading && (
+        <div className="flex items-center justify-end mt-1">
+          <TbClick size={20} />
+          <p className="mr-2">123</p>
+          <AiOutlineLike size={24} />
+          <p className="mr-2">{note.likes.length}</p>
+          <GrDownload size={20} />
+          <p className="mr-2">{note.numDownload}</p>
+          <BiCommentDetail size={24} />
+          <p className="mr-2">{note.comments.length}</p>
+        </div>
+      )}
     </div>
   );
 };
