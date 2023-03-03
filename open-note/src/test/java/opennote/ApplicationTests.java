6b687525d69c5785a6b2725de476d459cea7865c
TAKESHI <109770597+takeshi8989@users.noreply.github.com>
2023-03-03 14:06:47 -0800

    feat user add user info field (#150)
    * added info field
    
    * display user info

--- open-note/src/test/java/opennote/ApplicationTests.java
+++ open-note/src/test/java/opennote/ApplicationTests.java
@@ -13,9 +13,9 @@ import java.util.ArrayList;
 @SpringBootTest
 class ApplicationTests {
 
-	public static User user1 = new User(1, "user1", "user1@gmail.com", "password1", Role.USER, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
-	public static User user2 = new User(2, "user2", "user2@gmail.com", "password2", Role.USER, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
-	public static User user3 = new User(3, "user3", "user3@gmail.com", "password3", Role.USER, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+	public static User user1 = new User(1, "user1", "user1@gmail.com", "password1", "", Role.USER, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+	public static User user2 = new User(2, "user2", "user2@gmail.com", "password2", "",  Role.USER, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+	public static User user3 = new User(3, "user3", "user3@gmail.com", "password3", "",  Role.USER, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
 
 	public static Note note1 = new Note();
 	public static Note note2 = new Note();
