62b0724098f737119af04f90dea31cf13869d8dc
Takeshi Hashimoto <htakeshi0614@gmail.com>
2023-03-03 11:35:07 -0800

    send request when a note is clicked

--- frontend/src/components/SinglePagePDF.tsx
+++ frontend/src/components/SinglePagePDF.tsx
@@ -7,55 +7,65 @@ import { BiCommentDetail } from "react-icons/bi";
 import { GrDownload } from "react-icons/gr";
 import { TbClick } from "react-icons/tb";
 import CustomTag from "./tag/CustomTag";
+import { useRouter } from "next/router";
+import { useNote } from "@/hooks/useNote";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const SinglePagePDF = ({ note }: { note: Note }): JSX.Element => {
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [loadFailed, setLoadFailed] = useState<boolean>(false);
+  const { incrementViewCount } = useNote();
+
   const removeLoading = () => {
     setIsLoading(false);
+  };
+
+  const viewNote = async () => {
+    await incrementViewCount(note.id).then((res) => res);
+    router.push(`/note/${note.id}`);
   };
 
   return (
     <div className="mb-14 mt-14" style={{ width: 300 }}>
       <p className="text-center mb-0 pb-0 text-2xl">{note.title}</p>
       <p className="text-center mt-0">{note.user?.username}</p>
-      <Link href={`note/${note.id}`}>
-        {/* The first Page of PDF file */}
 
-        <div className="relative">
-          {isLoading && !loadFailed && (
-            <div
-              className="flex justify-center items-center"
-              style={{ width: "300px", height: "300px" }}
-            >
-              <Loading size="lg" />;
-            </div>
-          )}
-          <Document
-            file={note.url}
-            onLoadSuccess={removeLoading}
-            loading=""
-            error=""
-            onLoadError={() => setLoadFailed(true)}
+      {/* The first Page of PDF file */}
+
+      <div className="relative cursor-pointer" onClick={viewNote}>
+        {isLoading && !loadFailed && (
+          <div
+            className="flex justify-center items-center"
+            style={{ width: "300px", height: "300px" }}
           >
-            <Page
-              className="w-full"
-              pageNumber={1}
-              renderTextLayer={false}
-              renderAnnotationLayer={false}
-              width={300}
-            />
-          </Document>
-          {/* 2 Tags */}
-          {!isLoading && (
-            <div className="flex absolute bottom-3 right-1">
-              {note.tags.length > 0 && <CustomTag tag={note.tags[0]} />}
-              {note.tags.length > 1 && <CustomTag tag={note.tags[1]} />}
-            </div>
-          )}
-        </div>
-      </Link>
+            <Loading size="lg" />;
+          </div>
+        )}
+        <Document
+          file={note.url}
+          onLoadSuccess={removeLoading}
+          loading=""
+          error=""
+          onLoadError={() => setLoadFailed(true)}
+        >
+          <Page
+            className="w-full"
+            pageNumber={1}
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+            width={300}
+          />
+        </Document>
+        {/* 2 Tags */}
+        {!isLoading && (
+          <div className="flex absolute bottom-3 right-1">
+            {note.tags.length > 0 && <CustomTag tag={note.tags[0]} />}
+            {note.tags.length > 1 && <CustomTag tag={note.tags[1]} />}
+          </div>
+        )}
+      </div>
+
       {/* View, Like, Download, Commnet */}
       {!isLoading && (
         <div className="flex items-center justify-end mt-1">
