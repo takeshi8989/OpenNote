b144f84fabb62c021d5ba3efbcb2e4b57b091399
Takeshi Hashimoto <htakeshi0614@gmail.com>
2023-03-06 15:29:15 -0800

    search with enter key

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home = () => {
   const { setNoteListBySearch } = useNote();
   useEffect(() => {
     setNoteListBySearch();
-  }, [searchQuery]);
+  }, []);
 
   return (
     <div className="h-screen">
